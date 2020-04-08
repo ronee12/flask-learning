d5cd688ea93d78b3abed136730e52d439d1f977c
Mehedi Hasan Ronee <ron.mehedi@gmail.com>
2020-04-08 18:56:22 +0600

    finish chapter 4, learn about SQLalchemy database

--- microblog/config.py
+++ microblog/config.py
@@ -1,4 +1,8 @@
 import os
+baseDir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(baseDir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
